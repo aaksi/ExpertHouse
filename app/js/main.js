0bec0ba4137ee1eff0bf10c968b1021df65ac846
aaksi <yyff91@mail.ru>
2021-01-30 12:58:19 +0300

    added section top

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,16 @@
 
 $(function(){
-   
+
+    $('.top__slider').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        prevArrow: false,
+        nextArrow:false,
+        dots:true,
+    });
+
+    $("#phone").mask("+7 ( 9 9 9 ) 9 9 9 - 9 9 - 9 9");
+
 
 });
