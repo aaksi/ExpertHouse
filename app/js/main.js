a26223184287e8d25a710f2ada1e7ff70eb12e56
aaksi <yyff91@mail.ru>
2021-01-31 16:30:18 +0300

    added section team

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,18 @@ $(function(){
         dots:true,
     });
 
+    
+$('.team__slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    prevArrow:'<button class="slider__btn slider__btn--left"></button>',
+    nextArrow:'<button class="slider__btn  slider__btn--right"></button>',
+  });
+      
+
     $("#phone").mask("+7 ( 9 9 9 ) 9 9 9 - 9 9 - 9 9");
     $("#phone2").mask("+7 ( 9 9 9 ) 9 9 9 - 9 9 - 9 9");
 
